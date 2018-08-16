a0d1bbe875c1045a7b87491b7cb247d03ddae6c6
Nehal Udyavar <nehaludyavar@gmail.com>
2018-08-16 22:13:11 +0400

    Convert Panel from stateless function to Component

--- src/components/items/Panel.js
+++ src/components/items/Panel.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default function Panel({ title, sizeClass, info }) {
-  return (
-    <div className={ sizeClass }>
-      <h3 className='header--3'>{ title }</h3>
-      <div className='panel'>
-        <div className='panel-body'>
-          Hello there
-        </div>
-        <div className='panel-body'>
-          This is the second one
+class Panel extends Component {
+  render() {
+    const { title, sizeClass } = this.props
+    
+    return (
+      <div className={ sizeClass }>
+        <h3 className='header--3'>{ title }</h3>
+        <div className='panel'>
+          <div className='panel-body'>
+            Hello there
+          </div>
+          <div className='panel-body'>
+            This is the second one
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+function mapStateToProps({ courses }, { title, sizeClass }) {
+  return {
+    title,
+    sizeClass
+  }
+}
+
+export default connect(mapStateToProps)(Panel)
