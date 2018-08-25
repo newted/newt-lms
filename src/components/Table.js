5fef0548c8d001440040a1b0b0997946dcbbe0f7
Nehal Udyavar <nehaludyavar@gmail.com>
2018-08-25 13:42:45 +0530

    Change table status representation from text to circle symbol

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,25 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { FaCircle } from 'react-icons/fa'
+
+const StatusIcon = ({ data }) => {
+  const status = data.status
+  let color = ''
+
+  if (status === 'Complete') {
+    color = 'lightgreen'
+  } else if (status === 'In Progress') {
+    color = 'lightgrey'
+  } else {
+    color = 'red'
+  }
+
+  return (
+    <td>
+      <FaCircle color={ color }/>
+    </td>
+  )
+}
 
 class Table extends Component {
   render() {
@@ -21,9 +41,17 @@ class Table extends Component {
                 { Object.keys(fields).map((header) => {
                   const key = fields[header]
 
-                  return (
-                    <td key={ object['assignmentId'] + key }>{ object[key] }</td>
-                  )
+                  if (key === 'status') {
+                    return <StatusIcon
+                      key={ object['assignmentId'] + key }
+                      data={ object } />
+                  } else {
+                    return (
+                      <td key={ object['assignmentId'] + key }>
+                        { object[key] }
+                      </td>
+                    )
+                  }
                 })}
               </tr>
             ))}
