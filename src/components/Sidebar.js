3c2a9634923ba2b9bcc14c79081af91408fdc875
Nehal Udyavar <nehaludyavar@gmail.com>
2018-08-28 13:26:57 +0530

    Add icons to sidebar navlinks

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
+// Icons
+import { MdDashboard } from 'react-icons/md'
+import { FiBook } from 'react-icons/fi'
+import { IoIosSchool } from 'react-icons/io'
+import { TiPencil } from 'react-icons/ti'
 
 class Sidebar extends Component {
   render() {
@@ -9,22 +14,34 @@ class Sidebar extends Component {
           <ul>
             <li className='navlink'>
               <NavLink to='/' exact activeClassName='active-nav'>
-                Dashboard
+                <div className='navlink--row'>
+                  <MdDashboard className='navlink--icon' size={ 20 }/>
+                  <div>Dashboard</div>
+                </div>
               </NavLink>
             </li>
             <li className='navlink'>
               <NavLink to='/courses' activeClassName='active-nav'>
-                Courses
+                <div className='navlink--row'>
+                  <FiBook className='navlink--icon' size={ 20 }/>
+                  <div>Courses</div>
+                </div>
               </NavLink>
             </li>
             <li className='navlink'>
               <NavLink to='/assignments' activeClassName='active-nav'>
-                Assignments
+                <div className='navlink--row'>
+                  <IoIosSchool className='navlink--icon' size={ 20 }/>
+                  <div>Assignments</div>
+                </div>
               </NavLink>
             </li>
             <li className='navlink'>
               <NavLink to='/quizzes' activeClassName='active-nav'>
-                Quizzes
+                <div className='navlink--row'>
+                  <TiPencil className='navlink--icon' size={ 20 }/>
+                  <div>Quizzes</div>
+                </div>
               </NavLink>
             </li>
           </ul>
