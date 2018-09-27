16d86e17d4adb9bacaf9219c6850841557542030
Nehal Udyavar <nehaludyavar@gmail.com>
2018-09-27 15:29:24 -0700

    Logout button UI on Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react'
 class Navbar extends Component {
   render() {
     return (
-      <nav className='navbar'>
-        Navbar
+      <nav className='navbar-container'>
+        <div className='navbar'>
+          <div>Navbar</div>
+          <button className='button logout-button'>Logout</button>
+        </div>
       </nav>
     )
   }
