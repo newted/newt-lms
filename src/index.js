00410061469825af164cebb2c8f09da6d79c8d3c
Nehal Udyavar <nehaludyavar@gmail.com>
2018-10-15 13:11:39 -0700

    Add AWS config file + configure AWS with Amplify

--- src/index.js
+++ src/index.js
@@ -2,10 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import Amplify from 'aws-amplify'
+import config from './aws-config'
 import './index.css'
 import App from './containers/App'
 import reducer from './reducers'
 import middleware from './middleware'
+
+// Configure AWS
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "newt-api-trial",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      }
+    ]
+  }
+})
 
 const store = createStore(reducer, middleware)
 
